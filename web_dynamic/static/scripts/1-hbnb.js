3791fc820039fd180231a7a1ef0c151d43f93b5b
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-27 00:16:07 +0100

    Task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,8 +1,13 @@
-// if($(".amenities li" ).is(":checked"))
-// $('input').change(function () {
-// if ($('input').is(':checked')) {
 $(document).ready(function () {
-  console.log('Hello');
+  let amenities = {};
+
+  $('.amenities .popover input').change(() => {
+    if ($(this).checked) {
+      amenities[$(this).attr("data-name")] = $(this).attr('data-id');
+    } else if ($(this).checked) {
+      delete amenities[$(this).attr('data-name')];
+    }
+    let names = Object.keys(amenities);
+    if (names.length > 0) {$('.amenities h4').text(names).sort();}
+  });
 });
-// }
-// });
