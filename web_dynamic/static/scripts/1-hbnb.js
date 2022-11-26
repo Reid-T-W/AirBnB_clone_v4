f3619a04a2af0784a622ffd8a6b4b20b16af3136
Reid-T-W <rediettadesse100@gmail.com>
2022-11-27 00:10:45 +0300

    Task 2: handling amenities checkboxes

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,8 @@
+// if($(".amenities li" ).is(":checked"))
+// $('input').change(function () {
+// if ($('input').is(':checked')) {
+$(document).ready(function () {
+  console.log('Hello');
+});
+// }
+// });
