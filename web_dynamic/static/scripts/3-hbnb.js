8f997ea1bf0d8e7abb8a894423ee61806030c03c
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-27 20:35:25 +0100

    Task 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -22,4 +22,17 @@ $(document).ready(function () {
       $('div#api_status').removeClass('available');
     }
   });
+
+  $.ajax({
+    type: "POST",
+    url: "http://0.0.0.0:5001/api/v1/places_search/",
+    data: "{}",
+    dataType: "json",
+    contentType: "application/json",
+    success: (data) => {
+      data.forEach(place => {
+        $('.places').append(`<article><div class="title_box"><h2>${place.name}</h2><div class="price_by_night">$${place.price_by_night}</div></div><div class="information"><div class="max_guest">${place.max_guest} Guests</div><div class="number_rooms">${place.number_rooms} Bedrooms</div><div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div></div><div class="description">$${place.description}</div></article>`);
+      });
+    }
+  });
 });
