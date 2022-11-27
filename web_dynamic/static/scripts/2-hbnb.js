bcae262f4b9a40977bd7c22343bd5fdd6747ec37
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-27 16:14:26 +0100

    task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,30 @@
 $(document).ready(function () {
+  const amenities = {};
+
+  $('.amenities .popover input').click(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-name')] = $(this).attr('data-id');
+    } else {
+      delete amenities[$(this).attr('data-name')];
+    }
+    const names = Object.keys(amenities);
+    if (names.length > 0) {
+      $('.amenities h4').text(names).sort();
+    } else {
+      $('.amenities h4').text('');
+    }
+  });
+
   fetch('http://localhost:5001/api/v1/status/')
-    //.then((response) => response.json())
-    .then((response) => console.log(response));
+  .then((response) => response.json()).then(data => {
+    if (data.status === "OK") {
+      $('#api_status').addClass('available');
+      // if ($('#api_status').hasClass('available')) {
+      //   console.log('yes it does');
+      // }
+    } else {
+      $('#api_status').removeClass('available');
+    }
+    // console.log(data);
+  });
 });
